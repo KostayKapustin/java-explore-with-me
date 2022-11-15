4cd89249f7a5a65e791498b0a9589ae9a624bd90
Костя Капустин <98394512+KostayKapustin@users.noreply.github.com>
2022-11-15 23:18:52 +0300

    Исправлена ошибка 3.

--- ewm-status/src/test/java/ru/practicum/ewmstat/EwmStatApplicationTests.java
+++ ewm-status/src/test/java/ru/practicum/ewmstat/EwmStatApplicationTests.java
@@ -1,9 +1,9 @@
 package ru.practicum.ewmstat;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@ContextConfiguration
 class EwmStatApplicationTests {
 
     @Test
